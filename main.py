e79f00cdf9633f506df5f2d87a98de28aaaa162e
Venkata-Vineela <kanamarlapudi.usedu1@gmail.com>
2023-10-25 21:59:38 -0500

    this is from local

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import logging
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import handlejsonfile
+import sqlite3
 
 logging.basicConfig(filename='app.log', level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
@@ -13,6 +14,33 @@ CORS(app)  # Enable CORS to allow cross-origin requests
 #     'user1': {'password': 'password1'},
 #     'user2': {'password': 'password2'}
 # }
+
+
+# Define the database initialize function
+def initialize_database():
+    conn = sqlite3.connect('user_data.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY,
+            firstname TEXT,
+            lastname TEXT,
+            username TEXT UNIQUE,
+            password TEXT,
+            phone TEXT,
+            organization TEXT,
+            street_address TEXT,
+            city TEXT,
+            state TEXT,
+            zipcode TEXT
+        )
+    ''')
+    conn.commit()
+    conn.close()
+
+# Initialize the database
+initialize_database()
+
 
 @app.before_request
 def log_request_info():
@@ -37,12 +65,10 @@ def login():
     data = request.get_json()
     username = data.get('username')
     password = data.get('password')
-    if handlejsonfile.authenticate(username,password):
+    if handlejsonfile.authenticate(username, password):
         return jsonify({"message": "Login successful"})
     else:
         return jsonify({"message": "Login failed"}), 401
-
-
 
 # Logout endpoint
 @app.route('/logout')
@@ -72,8 +98,8 @@ def signup():
     else:
         handlejsonfile.add_user(firstname, lastname, username, password, phone, organization, street_address, city, state, zipcode)
 
-    logging.info('firstname is: %s, lastname: %s, phone: %s, organization: %s',firstname,lastname,phone,organization)
-    logging.info(', street: %s, city: %s, state: %s, zipcode: %s',street_address,city,state,zipcode)
+    logging.info('firstname is: %s, lastname: %s, phone: %s, organization: %s', firstname, lastname, phone, organization)
+    logging.info(', street: %s, city: %s, state: %s, zipcode: %s', street_address, city, state, zipcode)
     logging.info('username in signup : %s', username)
     logging.info('password fetched from data: %s', password)
 
@@ -83,4 +109,4 @@ def signup():
 
 if __name__ == '__main__':
     print("started app")
-    app.run(host='0.0.0.0', port=5000,threaded=True)
+    app.run(host='0.0.0.0', port=5000, threaded=True, debug=False)
